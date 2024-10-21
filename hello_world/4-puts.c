bdefffa16c63675eefbe4facab91fa8e3ceb2586
sira-djam <b.soileux@orange.fr>
2024-10-21 13:35:33 +0000

    commande 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,17 +1,5 @@
 # include  <stdio.h>
 
- /** 
-* main - Point d'entrée 
-* Retour : Toujours 0 (Succès) 
-*/ 
-
-int  main ( void )
- { 
- puts ( "\"La programmation, c'est comme construire un puzzle multilingue" ); 
-
-  return ( 0 ); 
-}a programmation, c'est comme construire un puzzle multilingue#include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
