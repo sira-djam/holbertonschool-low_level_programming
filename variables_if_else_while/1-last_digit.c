db240dd1c516117d73aead600660bb9a00c98101
sira-djam <b.soileux@orange.fr>
2024-10-22 19:56:21 +0000

    commande 1-last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * returne always
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (last digit of n>5)
+{
+printf("%d and is greater than 5\n" , n) ;
+}
+if (last digit of n=0)
+{
+printf("%d and is 0\n" , n) ;
+}
+if (last difit of n<6 ,
+	return (0);
+}
