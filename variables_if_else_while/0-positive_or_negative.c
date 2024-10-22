dbaf45cda338e62ba405e8c2e5b0adb7507bba06
sira-djam <b.soileux@orange.fr>
2024-10-22 13:39:58 +0000

    premiere variable avec if

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n<0)
+	{
+		printf("\nis negative")
+	}
+	if(n>0)
+	{
+		printf("\nis positive")
+	}
+	if(n=0)
+	{
+		printf("\nis zero")
+	}
+	return (0);
+}
