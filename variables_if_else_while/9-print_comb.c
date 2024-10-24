e27ba7545d9c10b4af121cec01e8b3b742cc1c9f
sira-djam <b.soileux@orange.fr>
2024-10-24 12:00:51 +0000

    comannde mettre une virgule entre chaque chieffre a un nbr

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,13 +6,14 @@
 int main(void)
 {
 int n;
-for (n = 0; n <= 10; n++)
+for (n = 0; n <= 9; n++)
 {
 putchar(n + '0');
-}
-if (n <= 10)
+if (n < 9)
 {
 putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
