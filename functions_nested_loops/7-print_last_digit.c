5d4f556d16cbda926b9bb1e3ee53f089e0f7c250
sira-djam <b.soileux@orange.fr>
2024-10-25 19:28:52 +0000

    main8

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,14 @@
  */
 int _print_last_digit(int r)
 {
-	int last_digit = r % 10;
+ int last;
+        last = r % 10;
 
-	if (last_digit < 0)
-		last_digit *= -1;
+        if (last < 0)
+	{
+                last *= last -1;
+	}
+	       	_putchar(last + '0');
 
-	_putchar(last_digit + '0');
-
-	return (last_digit);
+        return (last);
 }
