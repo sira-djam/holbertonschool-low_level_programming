1518aa7494c33c7aeb372349a2e7e22f2f12aaae
sira-djam <b.soileux@orange.fr>
2024-10-25 12:04:09 +0000

    boucle dans une boucle alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+	c = 'a';
+	n = 1;
+	
+	while (n <= 10) 
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		n++;
+	}
+	
+	
+	
+}
+
