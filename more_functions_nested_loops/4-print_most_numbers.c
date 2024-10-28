ecea81192fbbff0e6d18d14b5d44829047c14224
sira-djam <b.soileux@orange.fr>
2024-10-28 15:48:41 +0000

    imprimer une suite sauf deux chiffres

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_most_number - imprimer une suite de 0 a 9 sauf deux chiffres"
+  *
+  * Return always
+  */
+void print_most_numbers(void);
+{
+	int n = 48;
+	for (n = 48; n < 58; n++)
+	{
+		if (n = 50 || n = 52)
+	{
+		continue;
+	}
+		putchar (n);
+	}
+	return (0)
+	}
