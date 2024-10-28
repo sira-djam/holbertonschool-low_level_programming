b04a56aa2db3b95f0864f4da898e21caad7ae633
sira-djam <b.soileux@orange.fr>
2024-10-28 13:43:02 +0000

    impirmer les chiffres entre 0 et 9

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include "main.h"
+/**
+* imprimer les chiffre compris ente 0 et 9
+* return 1 si vrai sinon return 0
+*/
+int _isdigit(int c)
+{
+if ((c >= 0) && (c <= 9))
+
+return (1);
+
+return (0);
+}
