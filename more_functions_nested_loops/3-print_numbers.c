36dca361fc9d490201d56e88bc362491fea76cd3
sira-djam <b.soileux@orange.fr>
2024-10-28 15:07:10 +0000

    imprimer suite de nbr 0ca 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print _print les chiffre dans l'ordre 0 a 9
+  *Return always
+  */
+void print_numbers(void)
+{
+int n = 48;
+for (n = 48; n < 58; n++)
+{
+_putchar(n);
+}
+_putchar(":n");
+}
+
+
