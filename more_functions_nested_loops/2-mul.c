32e0678327a8df6db5333c71c50cb5591b220538
sira-djam <b.soileux@orange.fr>
2024-10-28 14:40:04 +0000

    multiplication des entierss

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/*
-* multiplies two integers
-*@a: integers
-*@b: integers
-* Return always
+/**
+ *mul - multiplies two integers
+ *@a: integers
+ *@b: integers
+ *Return: always
 */
 int mul(int a, int b)
 {
 	return (a * b);
-}		
+}
