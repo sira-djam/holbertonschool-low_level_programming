2a48149d59dc394939ec5b1e2df6b0b5c0803881
sira-djam <b.soileux@orange.fr>
2024-10-29 12:35:30 +0000

    dessiner des #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_square - imprimer des #
+  * @size: taille qui doit etre prise en compte
+  * Return always
+  */
+void print_square(int size)
+
+int size
+{
+	while (size > 0)
+{
+	_putchar('#');
+}
+_putchar('\n');
+}
+
