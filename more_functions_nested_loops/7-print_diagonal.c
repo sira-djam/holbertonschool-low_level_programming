8e903c7568528f0f510184d75ad363bb9e0289a7
sira-djam <b.soileux@orange.fr>
2024-10-29 11:57:02 +0000

    fonction pour dessiner une diagonale

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_diagonal - dessiner grace a une fonction une diagonale
+  * @n: 
+  * Return alaways
+  */
+void print_diagonal(int n)
+	int n
+{
+	while (n > 0)
+	int a;
+	if (n <=  0)
+{
+	_putchar('\n');
+}
+	else
+{
+	for (a = 0; a < n; a++)
+{
+	_putchar('\');
+}
+_putchar('\n');
+}
+}
+
