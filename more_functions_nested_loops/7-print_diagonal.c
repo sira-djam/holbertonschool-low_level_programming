e234919613175f231f5786fdc8d0d81263f3379b
sira-djam <b.soileux@orange.fr>
2024-10-29 12:02:48 +0000

    fonction pour dessiner une diagonale

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,41 @@
 #include "main.h"
 /**
-  * print_diagonal - dessiner grace a une fonction une diagonale
-  * @n: 
-  * Return alaways
-  */
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the '\' char is printed
+ * Description: Can only use _putchar to print
+ */
 void print_diagonal(int n)
-	int n
 {
+	int c, i;
+
+	c = 0;
+
 	while (n > 0)
-	int a;
-	if (n <=  0)
+	{
+		i = c;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		c++;
+		n--;
+	}
+	if (c < 1)
+		_putchar('\n');
+
+
+
+
+
+
+
+
+
+
+
 {
 	_putchar('\n');
 }
