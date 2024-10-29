be9dc1dd10612737a6bc95ce5195604932d50e9f
sira-djam <b.soileux@orange.fr>
2024-10-29 08:53:23 +0000

    commande pour dessiner une ligne

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_line - realiser des lignes avec une fonction
+  * Return always
+  */
+void print_line(int n)
+	int n 
+{
+	while (n > 0)
+{
+	_putchar('_');
+}
+_putchar('\n');
+}
