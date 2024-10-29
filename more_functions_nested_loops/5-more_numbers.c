84afe94cc10152aaecf2849cb8b5afc1c1a26405
sira-djam <b.soileux@orange.fr>
2024-10-29 09:48:22 +0000

    imprimer plusieurs fois la meme suite de nombre

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * more_functions-nested_loops - imprimer la suite de nombre de 0 a 14 10 fois
+  * Return always
+  */
+void more_numbers(void)
+{
+	int r;
+	int n;
+	r = 0
+	while (r < 10) 
+	{
+	for (n >= 0 ; n <= 10 ; n++)
+	
+	for (n > 10 ; n <= 14; n1++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n')
+}
