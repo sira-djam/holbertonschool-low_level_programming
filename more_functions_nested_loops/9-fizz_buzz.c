926ae979e238972b1268995067b83f1093758dfd
sira-djam <b.soileux@orange.fr>
2024-10-29 19:37:37 +0000

    ecrire fizz buzz selon desresultat de multiple

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+  * fizz_buzz - impriméselon si multiple ou divisible les mots fizz ou buzz ou fizz buzz
+  * Return always
+  */
+fizz_buzz (int a)
+{
+	for (a = 1 ; a <= 100 ; a++)
+{
+	if (a % 15 == 0)
+	{
+		printf(FizzBuzz);
+	}
+	else if (a % 5 == 0)
+	{
+		printf(Buzz);
+	}
+	else if (a % 3 == 0)
+	{
+		printf(Fizz);
+	
+	}
+	else
+	{
+		printf(a)
+}
+}
+}
