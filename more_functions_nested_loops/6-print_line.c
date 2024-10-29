02c4ebde92cb876a72dda5809b9874696393a27d
sira-djam <b.soileux@orange.fr>
2024-10-29 09:25:24 +0000

    print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,23 @@
 #include "main.h"
 /**
   * print_line - realiser des lignes avec une fonction
+  * @n: number stock
   * Return always
   */
 void print_line(int n)
-	int n 
 {
-	while (n > 0)
+	int a;
+
+	if (n <=  0)
+{
+	_putchar('\n');
+}
+	else
+{
+	for (a = 0; a < n; a++)
 {
 	_putchar('_');
 }
 _putchar('\n');
+}
 }
