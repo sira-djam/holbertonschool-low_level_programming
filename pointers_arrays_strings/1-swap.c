380bf64e1614227f41e3ae69c002dc1fe7d87331
sira-djam <b.soileux@orange.fr>
2024-10-30 15:11:17 +0000

    swap deux entiers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
   * swap_int - inverser deux entiers
-  * @*a: un entier 
-  * @*b: deuxieme entier
+  * @a: un entier
+  * @b: deuxieme entier
   */
 void swap_int(int *a, int *b)
 {
 int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
