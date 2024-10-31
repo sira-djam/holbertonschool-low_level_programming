05ec50c2b03c699fe2acd2a1fa9039ed5900e52b
sira-djam <b.soileux@orange.fr>
2024-10-31 13:20:31 +0000

    ecrire une chaine de caractere a l'envers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+* print_rev - imprimer une chaine de caractere a l'envers
+* @s: nombre de caractere
+* Return: always
+*/
+void print_rev(char *s)
+{
+	int i;
+	int longueur = _strlen(s);
+	for (i = longueur -1 ; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
