68b78fa505cec6c702815b85b7712a16fa629f4f
sira-djam <b.soileux@orange.fr>
2024-10-31 12:01:34 +0000

    afficher la longeur de caracteres

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _strlen - afficher longueur d'une chaine de caractere
+  * @s: pointer
+  * Return: always
+  */
+
+int _strlen(char *s)
+{
+int longueur = 0;
+		while (longueur[s] != '\0')
+		{
+			longueur++;
+}
+		return (longueur);
+}
