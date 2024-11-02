9d5b2e1442b6312f28840c325beb453a64605096
sira-djam <b.soileux@orange.fr>
2024-11-02 14:59:15 +0000

    chaine de caractere avec pointer

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+/**
+  * *_strcpy - copier une chaine
+  * @*dest: 1er element du tableau
+  * @*src: nombre d'element dans le tableau
+  */
+
+  char *_strcpy(char *dest, char *src)
+
+ {
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
