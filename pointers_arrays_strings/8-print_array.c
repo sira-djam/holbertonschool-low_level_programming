449af1785a44daab12b63068a8a39e48ddc49970
sira-djam <b.soileux@orange.fr>
2024-11-02 08:38:35 +0000

    print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -6,15 +6,14 @@
   * @n: taille du tableau
   * @i: valeur de n
   */
-  void print_array(int *a, int n)
-
+void print_array(int *a, int n)
 {
 	int i;
 	for (i = 0; i < n; i++)
 	{
-		printf("%d" , a[i]);
-		if (i < n-1)
-		{
+		printf("%d", a[i]);
+		if (i < n - 1)
+	{
 			printf(", ");
 		}
 }
