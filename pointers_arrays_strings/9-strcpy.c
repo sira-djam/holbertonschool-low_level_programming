b55382f12d107b586d4c44a652dc3372cca87868
sira-djam <b.soileux@orange.fr>
2024-11-02 15:29:40 +0000

    chaine de caractere avec pointer

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,15 @@
 #include "main.h"
-#include <stddef.h>
+#include "2-strlen.c"
 /**
-  * *_strcpy - copier une chaine
-  * @*dest: 1er element du tableau
-  * @*src: nombre d'element dans le tableau
-  */
-char *_strcpy(char *dest, char *src)
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char*_strcpy(char *dest, char *src)
 {
 	char *start = dest;
 
