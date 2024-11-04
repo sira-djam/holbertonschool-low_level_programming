6022c1bb883e7e2d95042a56584ea1d8527152df
sira-djam <b.soileux@orange.fr>
2024-11-04 15:13:52 +0000

    1-strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -7,10 +7,11 @@
   8  * @src: string 2
   9  * Return: NULL if failure
  10  */
-char *_strncat(char *dest, char*src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0;
 	int dest_len = 0;
+
 	while (dest[index++])
 	dest_len++;
 	for (index = 0; src[index] && index < n; index++)
