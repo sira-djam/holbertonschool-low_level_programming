5f3d05c27dd3214d315ceb791892001196d31082
sira-djam <b.soileux@orange.fr>
2024-11-04 12:20:44 +0000

    fonction qui remplit la memoire d'octet

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
