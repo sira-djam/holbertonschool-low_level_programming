047c5e54bbc7463376cbd25f512606f0f9e6e721
sira-djam <b.soileux@orange.fr>
2024-11-04 13:15:12 +0000

    ecrire deux ligne l'une aprs l'autre

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,9 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0 ; dest[i] != \0 ; i++)
-	for (j = 0 ; src[j] != \0 , j++)
+	for (i = 0 ; dest[i] != '\0' ; i++)
+	for (j = 0 ; src[j] != '\0' ; j++)
 		dest[i] = src[j];
-	dest[i] = \0;
-	
-		return (dest);
-	}
-
-
-			
+	dest[i] = '\0';
+	return (dest);
+}
