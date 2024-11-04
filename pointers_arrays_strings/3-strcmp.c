a52cf4150a7388fc9e7713d320ce8fdf30e98c1e
sira-djam <b.soileux@orange.fr>
2024-11-04 20:37:09 +0000

    comparer deux chaine de caractere

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,22 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-	s1++;
-	s2++;
-	return (0);
-	}
-		return (*s1 - *s2);
+int c = 0;
+while (s1[c] == s2[c])
+{
+if (s1[c] == '\0' || s2[c] == '\0')
+{
+break;
 }
+c++;
+}
+if (s1[c] == '\0' && s2[c] == '\0')
+{
+return (0);
+}
+else
+{
+return (s1[c] - s2[c]);
+}
+}
+
