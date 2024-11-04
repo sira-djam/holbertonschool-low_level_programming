5f74a1ba9b76a33e70914caa5a8b5dec56797813
sira-djam <b.soileux@orange.fr>
2024-11-04 21:20:43 +0000

    ecrire les lettres minuscules en lettres majuscule

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * *string_toupper - changer les lettres minuscule en majuscule
+  */
+char *string_toupper(char *hi)
+{
+	int index = 0;
+	int desp = 'a' - 'A';
+
+	for (index = 0; hi[index] != '\0'; ++index)
+	{
+		if (hi[index] >= 'a' && hi[index] <= 'z')
+		{
+			hi[index] = hi[index] - desp;
+		}
+	}
+	return (hi);
+}
