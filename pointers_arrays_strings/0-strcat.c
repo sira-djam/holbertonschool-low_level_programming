1eb9d179d62a6b42042ea6719d497a12bb7a191e
sira-djam <b.soileux@orange.fr>
2024-11-04 13:05:31 +0000

    ecrire deux ligne l'une aprs l'autre

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * *_strcat - imprimer deux ligne a suivre
+  * @dest: est un pointer d'une chaine de caractere
+  * @src: deuxieme ligne de caractere pointer
+  * Return: dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; dest[i] != \0 ; i++)
+	for (j = 0 ; src[j] != \0 , j++)
+		dest[i] = src[j];
+	dest[i] = \0;
+	
+		return (dest);
+	}
+
+
+			
