a5da10cc5a1ae1e9e2cc4b1bca765403c65b0771
sira-djam <b.soileux@orange.fr>
2024-11-05 13:31:20 +0000

    fonction qui copie de la memoire

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _memcpy - copier une zone memoire vers une autre
+  * @dest: 1er zone memoire
+  * @src: 2eme zone memoire
+  * @n: nombre d'octet
+  * Return: always
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned i;
+
+for (i = 0 ; i < n ; i++)
+	dest[i] = src[i];
+		return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,3 +16,5 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
