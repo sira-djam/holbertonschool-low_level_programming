0952ddcfdfe532d970f4ad0f174a5b4ba89219df
sira-djam <b.soileux@orange.fr>
2024-11-07 14:11:41 +0000

    longueur d'un string avec recursive

---
+++ recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * factorial - utilisation recursive factorial
+  * @n: integers
+  */
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+	int factorial(int n)
+		n! = 0 (n + 1)
+
