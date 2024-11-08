0d684dd88a4d2e5217a901a61470feacbcc89475
sira-djam <b.soileux@gorange.fr>
2024-11-08 16:04:36 +0100

    argc argv nom de dossier

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the programm
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
