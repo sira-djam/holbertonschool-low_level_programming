d154c076a4a1a64aae6991a50dad54c956383491
sira-djam <b.soileux@gorange.fr>
2024-11-08 16:29:48 +0100

    2-args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int l;
+for (l = 0; l < argc; l++)
+{
+printf("%s\n", argv[l]);
+}
+return (0);
+}
