1714aff08e7271220e9f117de8a95782298a8299
sira-djam <b.soileux@gorange.fr>
2024-11-11 15:00:36 +0100

    create array with malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array -jcréer un tableau
+ * @size: taille du tableau
+ * @c: caractere specifique
+ * Return: pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+    char *a;
+    unsigned int i;
+    if (size == 0)
+    return (NULL);
+
+a = malloc (size * sizeof (*a));
+if (a == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+a[i] = c;
+return (a);
+    }
