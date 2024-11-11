b0a50fa1703e00cd2ec0484637f42d6db9d40ff0
sira-djam <b.soileux@gorange.fr>
2024-11-11 15:08:23 +0100

    create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,15 +8,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *a;
-    unsigned int i;
-    if (size == 0)
-    return (NULL);
+char *a;
+unsigned int i;
 
-a = malloc (size * sizeof (*a));
+if (size == 0)
+return (NULL);
+a = malloc(size * sizeof(*a));
 if (a == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
 a[i] = c;
 return (a);
-    }
+}
