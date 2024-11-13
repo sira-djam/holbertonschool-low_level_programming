cce1ada14e4f9f49b0ff5cb8c9a2dd464559a003
sira-djam <b.soileux@gorange.fr>
2024-11-13 13:18:52 +0100

    modif

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,15 +18,14 @@ int **alloc_grid(int width, int height)
         grid[i] = (int *)malloc(sizeof(int) * width);
         if (grid[i] == NULL)
         {
-            for (j = 0; j < i; j++)
-                free(grid[j]);
-            free(grid);
-            return NULL;
-        }
-
-        for (j = 0; j < width; j++)
+           for (j = 0; j < i; j++)
+           free(grid[j]);
+           free(grid);
+           return NULL;
+           }
+           for (j = 0; j < width; j++)
             grid[i][j] = 0;
-    }
+            }
+            return grid;
 
-    return grid;
 }
