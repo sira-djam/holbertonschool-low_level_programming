f6b45b250518997f5d69c91115455cdc9c7212c6
sira-djam <b.soileux@gorange.fr>
2024-11-14 14:13:28 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * create un type struct dog
+ * struct dog - create un type struct dog
+ * @name: nom
+ * @age: integers
+ * @owner: proprietaire
  */
 struct dog
 {
 char *name;
 float age;
-char *owner
-}
+char *owner;
+};
