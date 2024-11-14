bb96e78a73750ec13929068d6ebb9efd51bdde9f
sira-djam <b.soileux@gorange.fr>
2024-11-14 16:37:45 +0100

    2-print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,18 +9,16 @@ void print_dog(struct dog *d)
 
 if (d != NULL)
 {
-
     if (d->name != NULL)
     printf("Name: %s\n", d->name);
     else
-    printf("(nil)");
+    printf("Name: (nil)\n");
 
     printf("Age: %f\n", d->age);
 
     if (d->owner != NULL)
     printf("Owner: %s\n", d->owner);
     else
-    printf("(nil)");
-
+    printf("Owner: (nil)\n");
 }
 }
