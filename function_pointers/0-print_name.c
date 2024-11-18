b17e5a2df58cf94fd27ec6fd8a16f6da7a069743
sira-djam <b.soileux@gorange.fr>
2024-11-18 11:34:50 +0100

    pointer de fonction name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_name - pointer de fonction ecrire name
+ * @name: fonction qui permet d'ecrire le nom
+ * @f: pointer de fonction
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+
+{
+if (f !=0)
+	f(name);
+}
