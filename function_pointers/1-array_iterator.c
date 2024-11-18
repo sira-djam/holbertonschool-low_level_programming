ff1c3263734e82f2f8d682e5eab308e71d6a7b14
sira-djam <b.soileux@gorange.fr>
2024-11-18 13:00:01 +0100

    tableau iterer pointeur de fonction

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - iterer un tableau
+ * @array: tableau
+ * @size: taille de tableau
+ * @action: pointeur de fonction
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (size == o)
+	return;
+		for (i = 0; i < size; i++)
+		action (array[i]);
+}
