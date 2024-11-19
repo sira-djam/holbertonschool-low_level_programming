20f3714e3d01e6d668fc0ff878b4809e762a739f
sira-djam <b.soileux@gorange.fr>
2024-11-19 16:06:36 +0100

    dossier trois sur le soperandes

---
+++ function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include "calc.h"
+/**
+ *
+ */
+
+int main(argc, *argv[])
+
+int (*oprt)(int, int)
+
+if (argc != 4)
+{
+printf("error\n");
+exit (98);
+}
+if (!oprt)
+{
+printf("error\n")
+exit(99);
+}
+printf("%d\n", oprt(atoi(arv[1], atoi(argv[3])));
+return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_mul - multplie two integers
+ *@a: integers
+ *@b: integers
+ *Return: always
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_add - additionne two integers
+ *@a: integers
+ *@b: integers
+ *Return: always
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - soustrait two integers
+ *@a: integers
+ *@b: integers
+ *Return: always
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div -  division two integers
+ *@a: integers
+ *@b: integers
+ *Return: always
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - renvois le reste de la division de deux entiers
+ *@a: integers
+ *@b: integers
+ *Return: always
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
