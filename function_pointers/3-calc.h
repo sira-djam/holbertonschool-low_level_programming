8c4c3ea43c0ccae23152f2e895c3af295eb54009
sira-djam <b.soileux@gorange.fr>
2024-11-20 10:57:28 +0100

    task 3

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -6,10 +6,11 @@
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
-char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,26 +3,30 @@
 #include <stdlib.h>
 
 /**
+ * main - program to find an operation
+ * @argc: number of argument
+ * @argv: list of argument
  *
- */
-
-int main(int argc,char *argv[])
+ * Return: 0 Success
+*/
+int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int (*fptr)(int, int);
 
 	if (argc != 4)
 	{
-		printf("error\n");
+		printf("Error\n");
 		exit(98);
 	}
 
-	oprt = get_op_func(argv[2]);
+	fptr = get_op_func(argv[2]);
 
-	if (!oprt)
+	if (!fptr)
 	{
-		printf("error\n");
+		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+
+	printf("%d\n", fptr(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
