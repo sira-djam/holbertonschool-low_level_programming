635153e0e20bdea3c0226a42f424de18443613ce
sira-djam <b.soileux@gorange.fr>
2024-11-21 14:39:32 +0100

    modif dossie r2 int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,15 +8,19 @@
  */
 int int_index(int *array, int size, int (*cmp) (int))
 {
-	int i;
+	int i = 0;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-	return (-1);
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+				return (i);
+				i++;
+			}
+		}
 	}
-	for (i = 0; i < size; i++)
-	if (cmp(array[i]) != 0)
-	return (i);
-
 	return (-1);
 }
