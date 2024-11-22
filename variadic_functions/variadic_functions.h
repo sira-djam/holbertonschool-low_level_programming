7f99186e72c67b64cf5ed79c15b811893d0c5f92
sira-djam <b.soileux@gorange.fr>
2024-11-22 12:06:17 +0100

    faire le typedef de l'exercice 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,6 +5,14 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 int _putchar(char c);
+
+typedef struct tab
+{
+	char*spc;
+	void(*f)(va_list all);
+
+}print_type;
 
 #endif
