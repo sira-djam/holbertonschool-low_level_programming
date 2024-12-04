eb6f75431174d1fb97c893350c7cd6e934867871
sira-djam <b.soileux@gorange.fr>
2024-12-04 12:14:21 +0100

    4-free-_list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - free memory
+ * @head: tete de liste
+ */
+void free_list(list_t *head)
+
+{
+	list_t *f;
+	{
+	if (head == NULL)
+	return;
+	}
+	while (head != NULL)
+	{
+    f = head;
+    head = head->next;
+    free(f);
+	}
+	
+}
