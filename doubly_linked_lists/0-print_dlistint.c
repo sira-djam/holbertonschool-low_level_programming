b706960779feb19a577821a6186267b72e83e4e4
sira-djam <b.soileux@gorange.fr>
2024-12-05 14:34:16 +0100

    0-print_dlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,10 +3,19 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_dlistint
+ * print_dlistint - imprimer dans une liste double chainee
  * @h: pointeur
+ * Return: number of node
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+size_t i = 0;
 
+while (h)
+{
+	printf("%d\n", h->n);
+	i++;
+	h = h->next;
+}
+return (i);
 }
