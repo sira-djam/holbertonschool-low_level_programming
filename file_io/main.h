f758a57a08fb176a6f97cbf38473d89f41c10b5a
sira-djam <b.soileux@gorange.fr>
2024-12-06 14:47:56 +0100

    merc kiki

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H
+#define _MAIN_H
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 
