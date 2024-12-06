9ca957502270af8df83713e23e97b9ce4301756b
sira-djam <b.soileux@gorange.fr>
2024-12-06 10:40:46 +0100

    modif fichier 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,38 +11,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
 	unsigned int i;
+	dlistint_t *node, *curr;
 
-	if (h == NULL)
-	{
-		return (NULL);
-	}
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+	node = NULL;
 	if (idx == 0)
-		new_node->next = *h;
-	if (*h != NULL)
-		(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	current = *h;
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-		current = current->next;
-	if (current == NULL)
+		node = add_dnodeint(h, n);
+	else
 	{
-		return (NULL);
+		curr = *h;
+		i = 1;
+		while (curr)
+		{
+			if (i == idx)
+			{
+				if (!curr->next)
+					node = add_dnodeint_end(h, n);
+				else
+				{
+					node = malloc(sizeof(dlistint_t));
+					if (node)
+					{
+						node->n = n;
+						node->next = curr->next;
+						node->prev = curr;
+						curr->next->prev = node;
+						curr->next = node;
+					}
+				}
+				break;
+			}
+			curr = curr->next;
+			i++;
+		}
 	}
-	new_node->next = current->next;
-	new_node->prev = current;
-	if (current->next != NULL)
-		current->next->prev = new_node;
-		current->next = new_node;
-	return (new_node);
+	return (node);
 }
