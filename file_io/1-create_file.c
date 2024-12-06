fc3601883aa75efbe65e85546628fa1665d56787
sira-djam <b.soileux@gorange.fr>
2024-12-06 15:20:03 +0100

    1-creat_file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creer un fichier
+ * @filename: nom du fichier
+ * @text_content: text
+ * Return: always
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0;
+	ssize_t print;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	print = write(fd, text_content, strlen(text_content));
+	if (print == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -1,11 +1,13 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *text_content);
 
 #endif
